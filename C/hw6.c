d38925e1c7f2107d096f409c76c090038ebfec7c
Aditya Balwani <adibalwani@gmail.com>
2014-10-30 19:49:35 -0400

    Completed -i :D

--- C/hw6.c
+++ C/hw6.c
@@ -6,9 +6,44 @@
 #include <stdio.h>
 #define HELP "\t-h\tDisplays this help menu\n\t-i\tDisplays statistics about instructions type usages.\n\t-m\tDisplays all the immediate values used in I-Type and J-Type instructions\n\t-r\tDisplays information about the registers\n\t-u\tDisplays human readable headers for different information displayed.\n\t\tShould only be used with the -i, -r, and -m flags."
 #define ERROR_SUCCESS 0
+#define ERROR_REG 1
+#define ERROR_INSTR 2
+#define ERROR_IMMEDIATE 3
 #define ERROR_FLAGS 4
+
 void printHelp() {
   printf("%s\n",HELP);
+}
+
+void displayStatistics() {
+  int i=0, j=0, r=0, sum=0;
+  int readHex,opcode;
+  float avgi, avgj, avgr;
+  while(scanf("%x\n",&readHex) == 1) {
+    opcode = readHex >> 26;
+    if(opcode == 0) {
+      r++;
+    }
+    else if (opcode== 2 || opcode == 3) {
+      j++;
+    }
+    else{
+      i++;
+    }
+  }
+  sum = i+j+r;
+  avgi=((float)i/sum)*100;
+  avgj=((float)j/sum)*100;
+  avgr=((float)r/sum)*100;
+  printf("R-Type\t%d\t%.2f%%\nJ-Type\t%d\t%.2f%%\nI-Type\t%d\t%.2f%%\n",r,avgr,j,avgj,i,avgi);
+}
+
+void displayAll() {
+
+}
+
+void displayRegisters() {
+
 }
 
 int main(int argc, char *argv[]) {
@@ -39,6 +74,16 @@ int main(int argc, char *argv[]) {
     if(hflag){
       printHelp();
       return ERROR_SUCCESS;
+    }
+    else if(iflag) {
+      displayStatistics();
+      return ERROR_SUCCESS;
+    }
+    else if(mflag) {
+      displayAll();
+    }
+    else if(rflag) {
+      displayRegisters();
     }
   }
 }
