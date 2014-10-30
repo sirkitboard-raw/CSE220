cbf378793f071e926367e11d7e6fa67c942f73c0
Aditya Balwani <adibalwani@gmail.com>
2014-10-30 12:13:37 -0400

    Added help menu

--- C/hw6.c
+++ C/hw6.c
@@ -1,7 +1,17 @@
 #include<stdio.h>
+#define HELP "\t-h\tDisplays this help menu\n\t-i\tDisplays statistics about instructions type usages.\n\t-m\tDisplays all the immediate values used in I-Type and J-Type instructions\n\t-r\tDisplays information about the registers\n\t-uDisplays human readable headers for different information displayed.\n\t\tShould only be used with the -i, -r, and -m flags."
 
 int main(int argc, char *argv[]) {
   if(argc ==0) {
-    printf("")
+    printf("%s\n",HELP);
+    return 4;
+  }
+  else {
+    if(argc==1) {
+      if(argv[1]=='h'){
+        printf("%s\n",HELP);
+        return 0;
+      }
+    }
   }
 }
