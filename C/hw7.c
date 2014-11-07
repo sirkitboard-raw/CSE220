515407a51062250610ce701c995efa4e26804e7d
Aditya Balwani <adibalwani@gmail.com>
2014-11-06 20:24:34 -0500

    Fixed Debug

--- C/hw7.c
+++ C/hw7.c
@@ -8,7 +8,7 @@
 #include "hw7.h"
 #define HELP "usage: ./a.out [-s | -a | -h]\n\t-s\tSubstitution cipher\n\t\tAdditional parameters: [-e | -d] n INPUT_FILE OUTPUT_FILE\n\t\t-e\tEncrypt using the substitution cipher.\n\t\t-d\tDecrypt using the substitution cipher.\n\t\tn\tThe amount of position to shift by.\n\t\tINPUT_FILE This can be any file on the file system or - which specifies stdin.\n\t\tOUTPUT_FILE\tThis can be any file on the system or - which specifies stdout.\n\n\t-a\tAutokey cipher\t\tAdditional parameters: [-e | -d] n INPUT_FILE KEY_FILE OUTPUT_FILE\n\t\t-e\t\tEncrypt using the autokey cipher\n\t\t-d\t\tDecrypt using the autokey cipher\n\t\tn\t\tThe initial shift value\n\t\tINPUT_FILE\tThis can be any file on the file system or - which specifies stdin.\n\t\tKEY_FILE\tThis can be any file on the file system or - which specifies stdin.\n\t\tOUTPUT_FILE\tThis can be any file on the system or - which specifies stdout.\n\t-h\tDisplays this help menu\n"
 
-#ifdef DEBUG
+#ifdef CSE220
   #define debug(fmt, ...) printf("DEBUG:%s:%s:%d " fmt "\n", __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__)
 #else
   #define debug(fmt,...)
@@ -19,5 +19,6 @@ void printHelp() {
 }
 
 int main(int argc, char* argv[]) {
+  debug("HELLO!");
   return EXIT_SUCCESS;
 }
