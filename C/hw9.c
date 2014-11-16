50a33659f571fa3ec9dfee3f4e446c8f787f93ed
Aditya Balwani <adibalwani@gmail.com>
2014-11-16 18:43:55 -0500

    Attempted file reading

--- C/hw9.c
+++ C/hw9.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include "hw9.h"
 
-void main(int argc, char* argv[]) {
-  
+int main(int argc, char* argv[]) {
+
+  // char BUFFER[256];
+  // char type;
+  // int pretty, uid;
+  // FILE *fi = fopen("instruction_mapping.txt","r");
+  // while(!feof(fi)) {
+  //   if(fscanf(fi,"%c %x %s %d\n",&type, &uid, BUFFER, &pretty) ==4){
+  //     printf("%c %d %s %d\n",type, uid, BUFFER, pretty);
+  //   }
+  // }
+  // return 1;
 }
