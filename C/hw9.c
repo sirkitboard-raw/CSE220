892a703b6f3e52be35f7f374fab69cdc3b05da46
Aditya Balwani <adibalwani@gmail.com>
2014-11-20 22:22:23 -0500

    Almost complete

--- C/hw9.c
+++ C/hw9.c
@@ -2,10 +2,52 @@
 #include "hw9.h"
 #include <stdint.h>
 #include <string.h>
+#include <unistd.h>
 #include <netinet/in.h>
+#include <sys/stat.h>
 
 int reverseflag;
 char numHuman[32][5];
+FILE *fInstructStream=NULL,*fInStream=NULL, *fOutStream=NULL;
+InstrType *superhead = NULL;
+
+#ifdef CSE220
+  #define cse220(fmt, ...) printf("CSE220:" fmt "\n", ##__VA_ARGS__);
+#else
+  #define cse220(fmt,...)
+#endif
+
+void printMenu() {
+  printf("Usage: ./a.out [-h] [-m INSTRUCTION_MAPPING] -i INPUT_FILE -o OUTPUT_FILE\n\t-h\tDisplays this help menu.\n\t-m\tINSTRUCTION_MAPPING File that contains the instruction mapping.\n\t\tIf this option is not provided it defaults to instruction_mapping.txt.-\n\ti\tINPUT_FILE This should be a binary file that contains the .text section of a Mars220 MIPS binary.\n\t-o\tOUTPUT_FILE This can be any file on the system or \"-\" which specifies stdout.");
+}
+
+void freeInstrSpace(Instr* head) {
+  if(head==NULL) {
+    return;
+  }
+  Instr* temp = head->next;
+  free(head->mnemonic);
+  free(head);
+  freeInstrSpace(temp);
+}
+
+
+void freeTypeSpace(InstrType* head) {
+  if(head==NULL) {
+    return;
+  }
+  InstrType* temp = head->next;
+  freeInstrSpace(head->head);
+  free(head);
+  freeTypeSpace(temp);
+}
+
+void exitThingy(FILE* fins,FILE* fin,FILE* fout, InstrType*head) {
+  fclose(fins);
+  fclose(fout);
+  fclose(fin);
+  freeTypeSpace(head);
+}
 
 void initData() {
   strcpy(numHuman[0],"zero");
@@ -49,6 +91,10 @@ InstrType* initInstrTypeList(InstrType *head) {
   Instr *headInstr = NULL;
   size_t mallocType = sizeof(type)+sizeof(count)+(sizeof(InstrType*)*3);
   head = (InstrType*)(malloc(mallocType));
+  if(head == NULL) {
+    exitThingy(fInStream,fInstructStream, fOutStream, superhead);
+    exit(EXIT_FAILURE);
+  }
   cursor = head;
   (*cursor).type = 'R';
   (*cursor).count=0;
@@ -56,6 +102,10 @@ InstrType* initInstrTypeList(InstrType *head) {
   (*cursor).head = headInstr;
   temp=cursor;
   (*cursor).next = (InstrType*)(malloc(mallocType));
+  if(cursor->next == NULL) {
+    exitThingy(fInStream,fInstructStream, fOutStream, superhead);
+    exit(EXIT_FAILURE);
+  }
   cursor = (*cursor).next;
   (*cursor).type = 'I';
   (*cursor).count=0;
@@ -63,6 +113,10 @@ InstrType* initInstrTypeList(InstrType *head) {
   (*cursor).head = headInstr;
   temp = cursor;
   (*cursor).next = (InstrType*)(malloc(mallocType));
+  if(cursor->next == NULL) {
+    exitThingy(fInStream,fInstructStream, fOutStream, superhead);
+    exit(EXIT_FAILURE);
+  }
   cursor = (*cursor).next;
   (*cursor).type = 'J';
   (*cursor).count=0;
@@ -100,6 +154,10 @@ Instr* addInstrToList(Instr *head, uint32_t uid, char* name, uint32_t pretty) {
   cursor = head;
   if(cursor == NULL) {
     cursor = malloc(sizeof(uid) + sizeof(name) + sizeof(pretty) + (sizeof(Instr*)*2));
+    if(cursor == NULL) {
+      exitThingy(fInStream,fInstructStream, fOutStream, superhead);
+      exit(EXIT_FAILURE);
+    }
     cursor->uid = uid;
     cursor->mnemonic = name;
     cursor->pretty=pretty;
@@ -112,6 +170,10 @@ Instr* addInstrToList(Instr *head, uint32_t uid, char* name, uint32_t pretty) {
       if(strcmp(cursor->mnemonic,name)==1) {
         temp = cursor;
         cursor = malloc(sizeof(uid) + sizeof(name) + sizeof(pretty) + (sizeof(Instr*)*2));
+        if(cursor == NULL) {
+          exitThingy(fInStream,fInstructStream, fOutStream, superhead);
+          exit(EXIT_FAILURE);
+        }
         cursor->uid = uid;
         cursor->mnemonic = name;
         cursor->pretty=pretty;
@@ -125,6 +187,10 @@ Instr* addInstrToList(Instr *head, uint32_t uid, char* name, uint32_t pretty) {
       if(strcmp(cursor->mnemonic,name)==-1 && strcmp(cursor->next->mnemonic,name)>1) {
         temp = cursor->next;
         cursor->next = malloc(sizeof(uid) + sizeof(name) + sizeof(pretty) + (sizeof(Instr*)*2));
+        if(cursor->next == NULL) {
+          exitThingy(fInStream,fInstructStream, fOutStream, superhead);
+          exit(EXIT_FAILURE);
+        }
         cursor->next->uid = uid;
         cursor->next->mnemonic = name;
         cursor->next->pretty=pretty;
@@ -136,6 +202,10 @@ Instr* addInstrToList(Instr *head, uint32_t uid, char* name, uint32_t pretty) {
       cursor = cursor->next;
     }
     cursor->next = malloc(sizeof(uid) + sizeof(name) + sizeof(pretty) + (sizeof(Instr*)*2));
+    if(cursor->next == NULL) {
+      exitThingy(fInStream,fInstructStream, fOutStream, superhead);
+      exit(EXIT_FAILURE);
+    }
     cursor->next->uid = uid;
     cursor->next->mnemonic = name;
     cursor->next->pretty=pretty;
@@ -164,18 +234,20 @@ void addInstr(InstrType *head, char type, uint32_t uid, char* name, uint32_t pre
   }
 }
 
-void initInstrList(InstrType *head) {
+void initInstrList(InstrType *head, FILE *fi) {
   char BUFFER[256];
   char type;
   char *mnemo;
   uint32_t pretty, uid;
-  FILE *fi = fopen("instruction_mapping.txt","r");
   while(!feof(fi)) {
     if(fscanf(fi,"%c %x %s %u\n",&type, &uid, BUFFER, &pretty) ==4){
       mnemo = malloc(strlen(BUFFER));
+      if(mnemo == NULL) {
+        exitThingy(fInStream,fInstructStream, fOutStream, superhead);
+        exit(EXIT_FAILURE);
+      }
       strcpy(mnemo,BUFFER);
-      addInstr(head,type, uid, mnemo, pretty);
-      printf("%c %x %s %u\n",type, uid, BUFFER, pretty);
+      addInstr(head,type, uid, mnemo, pretty);\
     }
   }
 }
@@ -184,17 +256,15 @@ void initInstrList(InstrType *head) {
 int reverse(int i) {
   int ret=0,j;
   int buffer[4];
-  buffer[0] = (i >> 24) & 0x000000FF;
-  buffer[1] = (i >> 16) & 0x000000FF;
-  buffer[2] = (i >> 8) & 0x000000FF;
-  buffer[3] = i & 0x000000FF;
+  buffer[0] = (i >> 24) & 0xFF;
+  buffer[1] = (i >> 16) & 0xFF;
+  buffer[2] = (i >> 8) & 0xFF;
+  buffer[3] = i & 0xFF;
   ret = 0x00000000;
   for(j=3;j>=0;j--) {
     ret = ret<<8;
     ret+=buffer[j];
-    printf("%x ",ret);
   }
-  printf("\n");
   return ret;
 }
 
@@ -247,11 +317,11 @@ Instr* findInstr(int byte,InstrType *cursor) {
   }
 }
 
-void printByte(int i, InstrType* head) {
+void printByte(int i, InstrType* head, FILE* fout) {
   Instr* found;
   int rd,rs,rt,imm16, imm26,shamt;
   found = findInstr(i,head);
-  if(found==NULL) printf("\n");
+  if(found==NULL);
   else {
     rd = i>>11 & 0x0000001F;
     rt = i>>16 & 0x0000001F;
@@ -260,69 +330,130 @@ void printByte(int i, InstrType* head) {
     imm16 = i&0x0000ffff;
     imm26 = i&0x03ffffff;
     if(found->pretty == 0) {
-      printf("%s $%s\n",found->mnemonic,numHuman[rd]);
+      fprintf(fout,"%s $%s\n",found->mnemonic,numHuman[rd]);
     }
     else if(found->pretty == 1) {
-      printf("%s $%s, $%s\n",found->mnemonic,numHuman[rs],numHuman[rt]);
+      fprintf(fout,"%s $%s, $%s\n",found->mnemonic,numHuman[rs],numHuman[rt]);
     }
     else if(found->pretty == 2) {
-      printf("%s $%s, $%s, 0x%x\n",found->mnemonic,numHuman[rt],numHuman[rs], imm16);
+      fprintf(fout,"%s $%s, $%s, 0x%x\n",found->mnemonic,numHuman[rt],numHuman[rs], imm16);
     }
     else if(found->pretty == 3) {
-      printf("%s $%s, $%s, $%s\n",found->mnemonic,numHuman[rd],numHuman[rt], numHuman[rs]);
+      fprintf(fout,"%s $%s, $%s, $%s\n",found->mnemonic,numHuman[rd],numHuman[rt], numHuman[rs]);
     }
     else if(found->pretty == 4) {
-      printf("%s $%s, 0x%x($%s)\n",found->mnemonic,numHuman[rt], imm16,numHuman[rs]);
+      fprintf(fout,"%s $%s, 0x%x($%s)\n",found->mnemonic,numHuman[rt], imm16,numHuman[rs]);
     }
     else if(found->pretty == 5) {
-      printf("%s\n",found->mnemonic);
+      fprintf(fout,"%s\n",found->mnemonic);
     }
     else if(found->pretty == 6) {
-      printf("%s 0x%x\n",found->mnemonic,imm26);
+      fprintf(fout,"%s 0x%x\n",found->mnemonic,imm26);
     }
     else if(found->pretty == 7) {
-      printf("%s $%s, 0x%x\n",found->mnemonic,numHuman[rt], imm16);
+      fprintf(fout,"%s $%s, 0x%x\n",found->mnemonic,numHuman[rt], imm16);
     }
     else if(found->pretty == 8) {
-      printf("%s $%s, $%s, 0x%x\n",found->mnemonic,numHuman[rd], numHuman[rs],shamt);
+      fprintf(fout,"%s $%s, $%s, 0x%x\n",found->mnemonic,numHuman[rd], numHuman[rs],shamt);
     }
     else if(found->pretty == 9) {
-      printf("%s $%s, $%s, 0x%x\n",found->mnemonic,numHuman[rs],numHuman[rt], imm16);
+      fprintf(fout,"%s $%s, $%s, 0x%x\n",found->mnemonic,numHuman[rs],numHuman[rt], imm16);
     }
   }
 }
 
-void readFile(FILE *fp, InstrType* head) {
+void readFile(FILE *fp, FILE* fout, InstrType* head) {
   int i;
   fread(&i,sizeof(int),1,fp);
   while(!feof(fp)){
-    printf("0x%x\n",i);
     if(reverseflag) i = reverse(i);
-    printf("0x%x\n",i);
-    printByte(i, head);
+    printByte(i, head,fout);
     fread(&i,sizeof(int),1,fp);
   }
 }
 
+
 int main(int argc, char* argv[]) {
-  InstrType *head = NULL;
-  char bom[4];
-  int mLittleFlag = 0, fLittleFlag = 0;
-  head = initInstrTypeList(head);
-  initInstrList(head);
-  printList(head);
-  if(htonl(42) != 42) {
-    mLittleFlag = 1;
+  int hflag=0, iflag=0, oflag=0,fLittleFlag = 0,mLittleFlag = 0;
+  char insFile[256], inFile[256], outFile[256], bom[4];
+  char c;
+  struct stat ins, in, out;
+  strcpy(insFile,"instruction_mapping.txt");
+  while((c= getopt(argc,argv,"hm:i:o:")) != -1) {
+    switch(c) {
+      case 'h':
+        hflag=1;break;
+      case 'm':
+        strcpy(insFile,optarg);
+        break;
+      case 'i':
+        iflag=1;
+        strcpy(inFile, optarg);
+        break;
+      case 'o':
+        oflag=1;
+        strcpy(outFile,optarg);
+        break;
+      default:
+        printMenu();
+    }
   }
-  FILE *fp = fopen("program.bin","rb");
-  fread(&bom,sizeof(char),4,fp);
-  if(bom[0]!=0x57 || bom[1]!=0x6f || bom[2]!=0x6e || bom[3] != 0x67) {
-    fLittleFlag = 1;
+  if(hflag) {
+    printMenu();
   }
-  if(fLittleFlag!=mLittleFlag) {
-    reverseflag = 1;
+  if(!iflag || !oflag) {
+    exitThingy(fInStream,fInstructStream, fOutStream, superhead);
+    return EXIT_FAILURE;
   }
-  initData();
-  readFile(fp, head);
-  return 1;
+  else {
+    stat(outFile,&out);
+    stat(inFile,&in);
+    stat(insFile,&ins);
+    if(out.st_ino == in.st_ino || out.st_ino == ins.st_ino || ins.st_ino == in.st_ino) {
+      exitThingy(fInStream,fOutStream,fInStream,superhead);
+      return EXIT_FAILURE;
+    }
+    if(strcmp("-",outFile)==0) {
+      fOutStream = stdout;
+    }
+    else {
+      fOutStream = fopen(outFile,"w");
+    }
+
+    fInStream = fopen(inFile,"rb");
+    fInstructStream = fopen(insFile,"r");
+    stat(outFile,&out);
+    stat(inFile,&in);
+    stat(insFile,&ins);
+    if(out.st_ino == in.st_ino || out.st_ino == ins.st_ino || ins.st_ino == in.st_ino) {
+      exitThingy(fInStream,fOutStream,fInStream,superhead);
+      return EXIT_FAILURE;
+    }
+    fread(&bom,sizeof(char),4,fInStream);
+    if(fOutStream==NULL || fInStream==NULL || fInstructStream==NULL) {
+      return EXIT_FAILURE;
+    }
+    if(htonl(42) != 42) {
+      mLittleFlag = 1;
+    }
+    if(bom[0]!=0x57 || bom[1]!=0x6f || bom[2]!=0x6e || bom[3] != 0x67) {
+      fLittleFlag = 1;
+    }
+    else if(bom[0]!=0x67 || bom[1]!=0x6f || bom[2]!=0x6e || bom[3] != 0x57) {
+      fLittleFlag = 0;
+    }
+    else {
+      exitThingy(fInStream,fOutStream,fInStream,superhead);
+      return EXIT_FAILURE;
+    }
+    if(fLittleFlag!=mLittleFlag) {
+      reverseflag = 1;
+    }
+    superhead = initInstrTypeList(superhead);
+    initInstrList(superhead,fInstructStream);
+    initData();
+    readFile(fInStream,fOutStream, superhead);
+    exitThingy(fInStream,fInstructStream,fOutStream,superhead);
+    return EXIT_SUCCESS;
+  }
 }
